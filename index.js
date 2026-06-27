7308c5ff1cb0f45da3bbd601a4379d2a44071b2f
dkh <dylanhamera25@gmail.com>
2026-06-27 00:13:42 +0000

    everything except for global settings button sides of js

--- index.js
+++ index.js
@@ -1,6 +1,4 @@
 // npx vite serve
-
-// import
 import './style.css';
 import * as THREE from 'three';
 import { EffectComposer } from 'three/examples/jsm/postprocessing/EffectComposer.js';
@@ -11,6 +9,7 @@ import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 // setup
 const scene = new THREE.Scene();
 scene.background = new THREE.Color(0x000000);
+
 const camera = new THREE.PerspectiveCamera(
   55,
   window.innerWidth / window.innerHeight,
@@ -19,10 +18,9 @@ const camera = new THREE.PerspectiveCamera(
 );
 
 // settings
-const G = 1
-const SIM_SPEED = 10;
-
-const FREEZE_DISTANCE = 1000;
+let G = 1;
+let SIM_SPEED = 10;
+let FREEZE_DISTANCE = 1000;
 
 const BODY1_MASS = 14;
 const BODY2_MASS = 10;
@@ -42,14 +40,13 @@ const CAMERA_MAX_DISTANCE = 500;
 
 // terminal
 let terminalHistory = [];
-
 const terminalOutput = document.getElementById("terminal-output");
 let terminalInput = "";
 let simulationRunning = false;
 let simulationPaused = false;
 
 function updateTerminal() {
-terminalOutput.innerHTML = `
+  terminalOutput.innerHTML = `
 > <span style="color: orange">Three-Body</span> <span style="color: red">Simulation</span> v0.1
 > Type <span style="color: green">"help"</span> for commands
 ${colorize(terminalHistory.join("<br>"))}
@@ -58,7 +55,6 @@ ${colorize(terminalHistory.join("<br>"))}
 }
 
 function resetBodies() {
-
   sunSphere.position.copy(BODY1_START_POS);
   sunSphere2.position.copy(BODY2_START_POS);
   sunSphere3.position.copy(BODY3_START_POS);
@@ -76,7 +72,6 @@ function resetBodies() {
 }
 
 window.addEventListener("keydown", (event) => {
-
   if (event.key.length === 1 && terminalInput.length < 35) {
     terminalInput += event.key;
   }
@@ -86,7 +81,6 @@ window.addEventListener("keydown", (event) => {
   }
 
   if (event.key === "Enter") {
-
     const command = terminalInput.trim().toLowerCase();
 
     terminalHistory.push(`> ${terminalInput}`);
@@ -100,79 +94,56 @@ window.addEventListener("keydown", (event) => {
     }
 
     if (command === "run") {
-
       if (simulationRunning) {
         terminalHistory.push("Simulation already running.");
-      }
-
-      else {
+      } else {
         simulationRunning = true;
         simulationPaused = false;
         lastTime = performance.now();
-
         terminalHistory.push("Simulation started.");
       }
-
     }
 
     if (command === "restart") {
-
-    resetBodies();
-
-    simulationRunning = true;
-    simulationPaused = false;
-
-    terminalHistory.push("Simulation restarted.");
+      resetBodies();
+      simulationRunning = true;
+      simulationPaused = false;
+      terminalHistory.push("Simulation restarted.");
     }
 
     if (command === "pause") {
-
       if (!simulationRunning) {
         terminalHistory.push("Simulation is not running.");
-      }
-
-      else if (simulationPaused) {
+      } else if (simulationPaused) {
         terminalHistory.push("Simulation already paused.");
+      } else {
+        simulationPaused = true;
+        terminalHistory.push("Simulation paused.");
       }
-
-      else {
-      simulationPaused = true;
-      terminalHistory.push("Simulation paused.");
-       }
     }
 
     if (command === "resume") {
-
       if (!simulationRunning) {
         terminalHistory.push("Simulation is not running.");
-      }
-
-      else if (!simulationPaused) {
+      } else if (!simulationPaused) {
         terminalHistory.push("Simulation is not paused.");
-      }
-
-      else {
+      } else {
         simulationPaused = false;
         lastTime = performance.now();
-
         terminalHistory.push("Simulation resumed.");
       }
-
     }
 
     if (command === "reset") {
-
       resetBodies();
-
       simulationRunning = false;
       simulationPaused = false;
-
       terminalHistory.push("Simulation reset.");
     }
 
     if (command === "debug") {
       terminalHistory.push(
-      `Bodies: ${bodies.length} | Running: ${simulationRunning} | Paused: ${simulationPaused}`
+        `Bodies: ${bodies.length} | Running: ${simulationRunning} | Paused: ${simulationPaused}`
       );
     }
 
@@ -180,27 +151,18 @@ window.addEventListener("keydown", (event) => {
   }
 
   updateTerminal();
-
 });
 
 function colorize(text) {
   return text
-    .replaceAll(/\bhelp\b/g,
-      '<span style="color: green">help</span>')
-    .replaceAll(/\bclear\b/g,
-      '<span style="color: cyan">clear</span>')
-    .replaceAll(/\bdebug\b/g,
-      '<span style="color: gold">debug</span>')
-    .replaceAll(/\brestart\b/g,
-      '<span style="color: orange">restart</span>')
-    .replaceAll(/\bpause\b/g,
-      '<span style="color: mediumorchid">pause</span>')
-    .replaceAll(/\brun\b/g,
-      '<span style="color: limegreen">run</span>')
-    .replaceAll(/\bresume\b/g,
-      '<span style="color: white">resume</span>')
-    .replaceAll(/\breset\b/g,
-      '<span style="color: tomato">reset</span>');
+    .replaceAll(/\bhelp\b/g, '<span style="color: green">help</span>')
+    .replaceAll(/\bclear\b/g, '<span style="color: cyan">clear</span>')
+    .replaceAll(/\bdebug\b/g, '<span style="color: gold">debug</span>')
+    .replaceAll(/\brestart\b/g, '<span style="color: orange">restart</span>')
+    .replaceAll(/\bpause\b/g, '<span style="color: mediumorchid">pause</span>')
+    .replaceAll(/\brun\b/g, '<span style="color: limegreen">run</span>')
+    .replaceAll(/\bresume\b/g, '<span style="color: white">resume</span>')
+    .replaceAll(/\breset\b/g, '<span style="color: tomato">reset</span>');
 }
 
 updateTerminal();
@@ -209,31 +171,31 @@ updateTerminal();
 const tabs = document.querySelectorAll(".body-tab");
 
 tabs.forEach(tab => {
-    tab.addEventListener("click", () => {
-
-        tabs.forEach(tab => {
-            tab.classList.remove("active");
-        });
-
-        tab.classList.add("active");
-
+  tab.addEventListener("click", () => {
+    tabs.forEach(tab => {
+      tab.classList.remove("active");
     });
+    tab.classList.add("active");
+  });
 });
 
 // setup
 const renderer = new THREE.WebGLRenderer();
-renderer.setSize( window.innerWidth, window.innerHeight );
+renderer.setSize(window.innerWidth, window.innerHeight);
 renderer.setPixelRatio(window.devicePixelRatio);
-renderer.setAnimationLoop( animate );
-document.body.appendChild( renderer.domElement );
-const controls = new OrbitControls( camera, renderer.domElement );
+renderer.setAnimationLoop(animate);
+document.body.appendChild(renderer.domElement);
+
+const controls = new OrbitControls(camera, renderer.domElement);
 controls.zoomSpeed = CAMERA_ZOOM_SPEED;
 controls.minDistance = CAMERA_MIN_DISTANCE;
 controls.maxDistance = CAMERA_MAX_DISTANCE;
+
 const composer = new EffectComposer(renderer);
 
 // 3 bodies
-const sunGeometry = new THREE.SphereGeometry( 1, 32, 16 );
+const sunGeometry = new THREE.SphereGeometry(1, 32, 16);
+
 const sunMaterial = new THREE.MeshStandardMaterial({
   color: 0xffdd99,
   emissive: 0xffaa44,
@@ -284,7 +246,6 @@ const bodies = [
     acceleration: new THREE.Vector3(),
     frozen: false
   },
-
   {
     mesh: sunSphere2,
     mass: sun2Mass,
@@ -369,37 +330,23 @@ const gravityArrow3 = new THREE.ArrowHelper(
 scene.add(gravityArrow3);
 
 function updateArrows() {
-
   velArrow.position.copy(bodies[0].mesh.position);
-  velArrow.setDirection(
-    bodies[0].velocity.clone().normalize()
-  );
+  velArrow.setDirection(bodies[0].velocity.clone().normalize());
 
   gravityArrow.position.copy(bodies[0].mesh.position);
-  gravityArrow.setDirection(
-    bodies[0].acceleration.clone().normalize()
-  );
+  gravityArrow.setDirection(bodies[0].acceleration.clone().normalize());
 
   velArrow2.position.copy(bodies[1].mesh.position);
-  velArrow2.setDirection(
-    bodies[1].velocity.clone().normalize()
-  );
+  velArrow2.setDirection(bodies[1].velocity.clone().normalize());
 
   gravityArrow2.position.copy(bodies[1].mesh.position);
-  gravityArrow2.setDirection(
-    bodies[1].acceleration.clone().normalize()
-  );
+  gravityArrow2.setDirection(bodies[1].acceleration.clone().normalize());
 
   velArrow3.position.copy(bodies[2].mesh.position);
-  velArrow3.setDirection(
-    bodies[2].velocity.clone().normalize()
-  );
+  velArrow3.setDirection(bodies[2].velocity.clone().normalize());
 
   gravityArrow3.position.copy(bodies[2].mesh.position);
-  gravityArrow3.setDirection(
-    bodies[2].acceleration.clone().normalize()
-  );
-
+  gravityArrow3.setDirection(bodies[2].acceleration.clone().normalize());
 }
 
 // corona
@@ -439,58 +386,36 @@ function createStars(count, size) {
   const colors = [];
 
   for (let i = 0; i < count; i++) {
-
     let x, y, z;
 
     if (Math.random() < 0.15) {
-
-      const cluster = clusters[
-        Math.floor(Math.random() * clusters.length)
-      ];
-
+      const cluster = clusters[Math.floor(Math.random() * clusters.length)];
       x = cluster.x + (Math.random() - Math.random()) * cluster.radius;
       y = cluster.y + (Math.random() - Math.random()) * cluster.radius;
       z = cluster.z + (Math.random() - Math.random()) * cluster.radius;
-
     } else {
-
       const radius = 1500 + Math.random() * 1000;
-
       const theta = Math.random() * Math.PI * 2;
       const phi = Math.acos(Math.random() * 2 - 1);
 
       x = radius * Math.sin(phi) * Math.cos(theta);
       y = radius * Math.sin(phi) * Math.sin(theta);
       z = radius * Math.cos(phi);
-
     }
 
-       positions.push(x, y, z);
+    positions.push(x, y, z);
 
     const brightness = 0.2 + Math.pow(Math.random(), 0.3) * 0.8;
-
     const r = brightness;
     const g = brightness * (0.85 + Math.random() * 0.15);
     const b = brightness * (0.7 + Math.random() * 0.3);
 
-    colors.push(
-      r,
-      g,
-      b
-    );
+    colors.push(r, g, b);
   }
 
   const geometry = new THREE.BufferGeometry();
-
-  geometry.setAttribute(
-    'position',
-    new THREE.Float32BufferAttribute(positions, 3)
-  );
-
-  geometry.setAttribute(
-    'color',
-    new THREE.Float32BufferAttribute(colors, 3)
-  );
+  geometry.setAttribute('position', new THREE.Float32BufferAttribute(positions, 3));
+  geometry.setAttribute('color', new THREE.Float32BufferAttribute(colors, 3));
 
   const material = new THREE.PointsMaterial({
     size,
@@ -498,38 +423,15 @@ function createStars(count, size) {
     sizeAttenuation: false
   });
 
-  material.onBeforeCompile = (shader) => {
-    shader.fragmentShader = shader.fragmentShader.replace(
-      '#include <clipping_planes_fragment>',
-      `
-      #include <clipping_planes_fragment>
-
-      vec2 cxy = 2.0 * gl_PointCoord - 1.0;
-      float r = dot(cxy, cxy);
-
-      if (r > 1.0) discard;
-      `
-    );
-  };
-
   return new THREE.Points(geometry, material);
 }
 
-const stars1 = createStars(4000, 1);
-stars1.frustumCulled = false;
-scene.add(stars1);
-
-const stars2 = createStars(800, 3);
-stars2.frustumCulled = false;
-scene.add(stars2);
-
-const stars3 = createStars(100, 6);
-stars3.frustumCulled = false;
-scene.add(stars3);
+scene.add(createStars(4000, 1));
+scene.add(createStars(800, 3));
+scene.add(createStars(100, 6));
 
 // camera
 camera.position.set(0, 0, 65);
-
 controls.target.set(0, 0, 0);
 controls.update();
 
@@ -538,150 +440,75 @@ composer.addPass(renderScene);
 
 const bloomPass = new UnrealBloomPass(
   new THREE.Vector2(window.innerWidth, window.innerHeight),
-   2.0,
+  2.0,
   0.8,
   0.15
 );
+
 composer.addPass(bloomPass);
 
 let lastTime = 0;
 let firstFrame = true;
 
-window.addEventListener('resize', () => {
-
-  camera.aspect = window.innerWidth / window.innerHeight;
-  camera.updateProjectionMatrix();
-
-  renderer.setSize(
-    window.innerWidth,
-    window.innerHeight
-  );
-
-  composer.setSize(
-    window.innerWidth,
-    window.innerHeight
-  );
-
-});
-
 // animation
-function animate( time ) {
-
-    if (!simulationRunning) {
-      composer.render();
-      return;
-    }
-
-    if (simulationPaused) {
-      composer.render();
-      return;
-    }
-
-    if (firstFrame) {
-      lastTime = time;
-      firstFrame = false;
-    return;
-    }
-
-    // delta time
-    const dt = ((time - lastTime) / 1000) * SIM_SPEED;
-    lastTime = time;
-
-    controls.update();
-
-    // physics
-    for (const A of bodies) {
-      if (A.frozen) continue;
-
-      const totalForce = new THREE.Vector3();
-
-      for (const B of bodies) {
-        if (B.frozen) continue;
-        if (A === B) continue;
-        
-        const direction = new THREE.Vector3()
-          .subVectors(B.mesh.position, A.mesh.position);
-        const distance = Math.max(direction.length(), 2);
-        
-        direction.normalize();
-
-        const forceStrength =
-          G * A.mass * B.mass / (distance ** 2);
-        
-        const force = direction.clone()
-          .multiplyScalar(forceStrength);
-
-        totalForce.add(force);
-      }
-
-      // calculate acceleration Newton's Law
-      const acceleration = totalForce.clone()
-        .divideScalar(A.mass);
-
-      // store acceleration
-      A.acceleration.copy(acceleration);
-
-    }
-
-    for (const A of bodies) {
-      if (A.frozen) continue;
-      A.velocity.add(
-      A.acceleration.clone().multiplyScalar(dt)
-      );
-    }
-
-    const center = new THREE.Vector3();
-
-    center.add(sunSphere.position);
-    center.add(sunSphere2.position);
-    center.add(sunSphere3.position);
-
-    center.divideScalar(3);
-
-    for (const A of bodies) {
-      if (A.frozen) continue;
-      A.mesh.position.add(
-      A.velocity.clone().multiplyScalar(dt)
-      );
-      if (A.mesh.position.distanceTo(center) > FREEZE_DISTANCE) {
-        A.frozen = true;
-      }
-    }
-
-    // arrows
-    updateArrows();
-
-    // corona
-    corona.rotation.y += 0.001;
-    corona.rotation.x += 0.0003;
-
-    corona2.rotation.y += 0.001;
-    corona2.rotation.x += 0.0003;
-
-    corona3.rotation.y += 0.001;
-    corona3.rotation.x += 0.0003;
-
-    const pulse =
-    1 +
-    Math.sin(time * 0.001) * 0.02 +
-    Math.sin(time * 0.0023) * 0.01;
-
-    corona.scale.setScalar(pulse);
-    corona2.scale.setScalar(pulse);
-    corona3.scale.setScalar(pulse);
-
-    // distance effect
-    const distance = camera.position.length();
-    const t = Math.min(distance / 200, 1);
-
-    sunMaterial.emissiveIntensity = 2.5 - (1.2 * t);
-    sunMaterial2.emissiveIntensity = 2.5 - (1.2 * t);
-    sunMaterial3.emissiveIntensity = 2.5 - (1.2 * t);
-
-    // sun rotation
-    sunSphere.rotation.y += 0.002;
-    sunSphere2.rotation.y += 0.002;
-    sunSphere3.rotation.y += 0.002;
-
+function animate(time) {
+  if (!simulationRunning) {
     composer.render();
+    return;
+  }
+
+  if (simulationPaused) {
+    composer.render();
+    return;
+  }
+
+  if (firstFrame) {
+    lastTime = time;
+    firstFrame = false;
+    return;
+  }
+
+  const dt = ((time - lastTime) / 1000) * SIM_SPEED;
+  lastTime = time;
+
+  controls.update();
+
+  for (const A of bodies) {
+    if (A.frozen) continue;
+
+    const totalForce = new THREE.Vector3();
+
+    for (const B of bodies) {
+      if (B.frozen) continue;
+      if (A === B) continue;
+
+      const direction = new THREE.Vector3().subVectors(B.mesh.position, A.mesh.position);
+      const distance = Math.max(direction.length(), 2);
+      direction.normalize();
+
+      const forceStrength = (G * A.mass * B.mass) / (distance * distance);
+      totalForce.add(direction.multiplyScalar(forceStrength));
+    }
+
+    A.acceleration.copy(totalForce.divideScalar(A.mass));
+  }
+
+  for (const A of bodies) {
+    if (A.frozen) continue;
+    A.velocity.add(A.acceleration.clone().multiplyScalar(dt));
+    A.mesh.position.add(A.velocity.clone().multiplyScalar(dt));
+  }
+
+  updateArrows();
+
+  corona.rotation.y += 0.001;
+  corona2.rotation.y += 0.001;
+  corona3.rotation.y += 0.001;
+
+  const pulse = 1 + Math.sin(time * 0.001) * 0.02 + Math.sin(time * 0.0023) * 0.01;
+  corona.scale.setScalar(pulse);
+  corona2.scale.setScalar(pulse);
+  corona3.scale.setScalar(pulse);
+
+  composer.render();
 }
